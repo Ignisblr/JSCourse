32e29878d48780ff5737ac39f3693e012720c386
ignisblr <tanokina86@gmail.com>
2022-03-28 22:17:00 +0300

    Lesson 2 main task completed

---
+++ lesson2/main.js
@@ -0,0 +1,13 @@
+const title = 'Lesson 2';
+const screens = "Простые, Сложные, Интерактивные";
+const fullPrice = 1000;
+const adaptive = false;
+
+let screenPrice = 26;
+let rollback = Math.floor(Math.random() * 100) + 1;
+
+console.log(title, fullPrice, adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани” и “Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
+console.log(screens.toLowerCase().split());
+console.log(fullPrice * (rollback / 100));
