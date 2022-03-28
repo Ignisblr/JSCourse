01fc883011156b9d7e3760039b2e5859d4154db9
ignisblr <tanokina86@gmail.com>
2022-03-28 21:53:20 +0300

    Lesson 1 task completed

---
+++ lesson1/main.js
@@ -0,0 +1,11 @@
+const title = 'Lesson 1';
+const screens = ['screen 1', 'screen 2', 'screen 3'];
+const fullPrice = 1000;
+const adaptive = {name: 'adaptive'};
+
+let screenPrice;
+let rollback;
+
+alert(`This is ${title}`);
+
+console.log('Number of screen is: ', screens.length);
