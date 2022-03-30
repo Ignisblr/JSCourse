cfd689a716a155771207ee57ed1665d8d1aec9c7
ignisblr <tanokina86@gmail.com>
2022-03-30 12:39:44 +0300

    Lesson 2 updated

--- main.js
+++ main.js
@@ -6,7 +6,16 @@ const adaptive = false;
 let screenPrice = 26;
 let rollback = Math.floor(Math.random() * 100) + 1;
 
-console.log(title, fullPrice, adaptive);
+function getType(objArray) {
+
+    for (let i = 0; i < objArray.length; i++) {
+
+        console.log(typeof(objArray[i]));
+    }
+}
+
+getType([title, fullPrice, adaptive]);
+
 console.log(screens.length);
 console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани” и “Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
 console.log(screens.toLowerCase().split());
