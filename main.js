b9b57a95c7c2742985a4532e39c1255f2ed5b0a8
ignisblr <tanokina86@gmail.com>
2022-04-07 15:29:31 +0300

    Lesson 5

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
-const title = prompt("Как называется ваш проект?");;
-const screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
-const screenPrice = +prompt("Сколько будет стоить данная работа?");
-const adaptive = confirm("Нужен ли адаптив на сайте?");
-const rollback = Math.floor(Math.random() * 100) + 1;
+const isNumber = function (num) {
+
+    return !isNaN(parseFloat(num)) && isFinite(num);
+}
+
+let title = prompt("Как называется ваш проект?");
+
+while (typeof(title) !== 'string' || title.trim() === "") {
+
+    title = prompt("Как называется ваш проект?");
+}
+
+let screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+
+while (typeof(screens) !== 'string' || screens.trim() === "") {
+
+    screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+}
+
+let screenPrice = prompt("Сколько будет стоить данная работа?");
+
+while (!isNumber(screenPrice)) {
+
+    screenPrice = prompt("Сколько будет стоить данная работа?");
+}
+
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let rollback = Math.floor(Math.random() * 100) + 1;
 
 let service1 = 0;
 let service2 = 0;
@@ -29,7 +52,7 @@ function getTitle() {
 
 function getFullPrice(nextFunc) {
 
-    fullPrice = screenPrice + getAllServicePrices();
+    fullPrice = +screenPrice + getAllServicePrices();
 
     nextFunc();
 }
@@ -56,7 +79,7 @@ function getRollbackMessage() {
 
 const getAllServicePrices = function () {
 
-    const allServicePrices = servicePrice1 + servicePrice2;
+    const allServicePrices = parseFloat(servicePrice1) + parseFloat(servicePrice2);
     return allServicePrices;
 }
 
@@ -67,12 +90,34 @@ function serviceQuestion() {
         if (i === 0) {
 
             service1 = prompt('Какой дополнительный тип услуги нужен?');
-            servicePrice1 = +prompt('Сколько это будет стоить?'); 
+
+            while (typeof(service1) !== 'string' || service1.trim() === "") {
+
+                service1 = prompt('Какой дополнительный тип услуги нужен?');
+            }
+
+            servicePrice1 = prompt('Сколько это будет стоить?'); 
+            
+            while (!isNumber(servicePrice1)) {
+
+                servicePrice1 = prompt('Сколько это будет стоить?');
+            }
         }
         if (i === 1) {
 
             service2 = prompt('Какой дополнительный тип услуги нужен?');
-            servicePrice2 = +prompt('Сколько это будет стоить?'); 
+
+            while (typeof(service2) !== 'string' || service2.trim() === "") {
+
+                service2 = prompt('Какой дополнительный тип услуги нужен?');
+            }
+
+            servicePrice2 = prompt('Сколько это будет стоить?'); 
+
+            while (!isNumber(servicePrice2)) {
+
+                servicePrice2 = prompt('Сколько это будет стоить?');
+            }
         }
     }
 }
